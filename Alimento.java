f3a1e3a18c956d9ac3d75ad0ed811575858b96cc
David <rulateunmai@hotmail.com>
2016-01-08 10:38:01 +0100

    Añadida funcionalidad para visualizar información nutricional de los alimentos

---
+++ Alimento.java
@@ -0,0 +1,82 @@
+
+/**
+ * Write a description of class Alimento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alimento
+{
+    // Almacena el nombre del alimento.
+    private String nombreAlimento;
+    // Almacena las proteinas del alimento.
+    private float gramosProteinas;
+    // Almacena los carbohidratos del alimento.
+    private float gramosCarbohidratos;
+    // Almacena las grasas del alimento.
+    private float gramosGrasas;
+    // Almacena el macronutriente del alimento.
+    private String macronutriente;
+    // Almacena las calorias del alimento.
+    private float calorias;
+
+    /**
+     * Constructor for objects of class Alimento
+     */
+    public Alimento(String nombreAlimento, float gramosProteinas, float gramosCarbohidratos, float gramosGrasas)
+    {
+        this.nombreAlimento = nombreAlimento;
+        this.gramosProteinas = gramosProteinas;
+        this.gramosCarbohidratos = gramosCarbohidratos;
+        this.gramosGrasas = gramosGrasas;         
+        if (gramosProteinas >= 0 && gramosCarbohidratos >= 0 && gramosGrasas >= 0 && gramosProteinas < 100 && gramosCarbohidratos < 100 && gramosGrasas < 100) {
+            calorias = (gramosProteinas * 4) + (gramosCarbohidratos * 4) + (gramosGrasas * 9);
+        }        
+        if (gramosProteinas >= gramosCarbohidratos && gramosProteinas >= gramosGrasas) {
+            macronutriente = "Proteinas";
+            if (gramosProteinas == gramosCarbohidratos && gramosProteinas == gramosGrasas) {
+                macronutriente = "Proteinas, carbohidratos y grasas";
+            }
+            else if (gramosProteinas == gramosCarbohidratos) {
+                macronutriente = "Proteinas y carbohidratos";
+            }
+            else if (gramosProteinas == gramosGrasas) {
+                macronutriente = "Proteinas y grasas";
+            }
+        }
+        if (gramosCarbohidratos >= gramosProteinas && gramosCarbohidratos >= gramosGrasas) {
+            macronutriente = "Carbohidratos";
+            if (gramosCarbohidratos == gramosProteinas) {
+                macronutriente = "Carbohidratos y proteinas";
+            }
+            else if (gramosCarbohidratos == gramosGrasas) {
+                macronutriente = "Carbohidratos y grasas";
+            }
+        }
+        if (gramosGrasas >= gramosProteinas && gramosGrasas >= gramosCarbohidratos) {
+            macronutriente = "Grasas";
+            if (gramosGrasas == gramosProteinas) {
+                macronutriente = "Grasas y proteinas";
+            }
+            else if (gramosGrasas == gramosCarbohidratos) {
+                macronutriente = "Grasas y carbohidratos";
+            }
+        }
+    }
+
+    /**
+     * Metodo que nos enseña por pantalla el alimento y las proteinas, carbohidratos y grasas de este.
+     * Ademas tambien muestra sus calorias y componentes mayoritarios.
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void muestraDatos(String nombreAlimento)
+    {
+        System.out.println("Nombre: " + nombreAlimento);
+        System.out.println("Proteinas por cada 100 gramos: " + gramosProteinas);
+        System.out.println("Carbohidratos por cada 100 gramos: " + gramosCarbohidratos);
+        System.out.println("Grasas por cada 100 gramos: " + gramosGrasas);
+        System.out.println("Calorias: " + calorias);
+        System.out.println("Componente/s mayoritario/s: " + macronutriente);
+    }
+}
